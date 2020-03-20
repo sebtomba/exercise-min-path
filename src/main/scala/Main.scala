92c3ed4019158b61c134cd09cec16788128a458b
Sebastian Bach <sebtomba@gmail.com>
2020-03-20 19:03:30 +0100

    Implement the entry point (input/output) of the application

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,17 @@
 object Main {
   def main(args: Array[String]): Unit = {
+    val in = io.Source.stdin.getLines.toList
+    NumberTriangle
+      .valuesFromString(in)
+      .fold(
+        println("Input from standard input contains non-numeric characters")
+      ) { values =>
+        if (NumberTriangle.validateValues(values)) {
+          val result = NumberTriangle.searchMinPath(values)
+          val path = result.path.mkString(" + ")
+          println(s"Minimal path is: $path = ${result.sum}")
+        } else
+          println("Provided values don't form a valid triangle of numbers")
+      }
   }
 }
